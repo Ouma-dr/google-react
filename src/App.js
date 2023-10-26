63dc20a76b54689e2f3b196fd31411fe120668e3
Ouma-dr <oumaymadridi905@gmail.com>
2023-10-26 14:34:57 +0100

    fix

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import './App.css';
 import Home from './pages/Home';
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import {HashRouter, Routes, Route} from 'react-router-dom'
 import SearchPage from './pages/SearchPage';
 
 function App() {
   return (
     <div className="App">
-      <Router>
+      <HashRouter>
         <Routes>
           <Route exact path='/' element= <Home />/>
           <Route exact path='/search' element= <SearchPage />/>
         
         </Routes>
-      </Router> 
+      </HashRouter> 
        
     </div>
   );
